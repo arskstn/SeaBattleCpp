b4d41731af2bbcd324c1958d8424ddb65fb7f6b0
arskstn <97250524+arskstn@users.noreply.github.com>
2023-06-28 21:55:42 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <vector>
+#include<unistd.h>
+
+using namespace std;
+//unsigned int microsecond = 1000000;
+fstream ofile("current.txt");
+string x;
+void disp_call(string now){
+    system("clear");
+    int counter = 0;
+    int disp_res_x = 60, disp_res_y = 16;
+    for(int i = 0; i < disp_res_y; i++){
+        for(int j = 0; j < disp_res_x; j++){
+            char nowx = now[counter];
+            if(nowx == 't'){
+                cout << "-";
+            }
+            else if (nowx == 'b'){
+                cout << "X";
+            }
+            else{
+                cout << "*";
+            }
+            counter++;
+        }
+        cout << endl;
+    }
+}
+
+
+
+void call_menu(){
+    cout << "1. Новая игра!\n2. Продолжить игру!\n3. Об авторах\n4. Вывести требования\n5. Выход из игры" << endl;
+    int chosen = 0;
+    cout << "Выберите действие: ";
+    cin >> chosen;
+    cout << chosen << endl;
+    cout << endl;
+    switch(chosen){
+        case 1:
+            getline(ofile, x);
+            disp_call(x);
+            //cout << "haha new game" << endl;
+            //call_menu();
+            return;
+        case 2:
+            cout << "continue game" << endl;
+            call_menu();
+            return;
+        case 3:
+            cout << endl << "Данная игра разработана на языке c++ в рамках выполнения практического задания для группы 8И23. Авторы: Клишина Ксения (8И23 гр.) и Костин Арсений (8Е21 гр.). 2023 г. Томский Политехнический Университет, г. Томск." << endl;
+            call_menu();
+            return;
+        case 4:
+            cout << "Данная игра выполняется в консоли. Разрешение окна игры 60*20 пикселей. Сохранение происходит каждые 20 секунд автоматически." << endl;
+            call_menu();
+            return;
+        case 5:
+            exit(0);
+        default:
+            cout << endl << "Вы ввели некорректное значение, попробуйте снова" << endl;
+            call_menu();
+    }
+}
+
+int main() {
+    cout << "Добро пожаловать в Морской Бой!" << endl;
+    call_menu();
+    return 0;
+}
