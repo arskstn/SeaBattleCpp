ef9d9a999dc77ca32f8329c8cee86c068255521f
arskstn <97250524+arskstn@users.noreply.github.com>
2023-07-02 19:41:26 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,10 +2,45 @@
 #include <cstdlib>
 #include <fstream>
 #include <vector>
+#include <string>
 
 using namespace std;
-fstream ofile("current.txt");
-string x;
+
+ifstream fdef("default.txt"); //Текст, с полями по умолчанию
+ofstream sboard("scoreboard.txt");//Текст, с таблицей рекордов
+ofstream sboard1("currtime.txt");
+fstream fnow("current.txt"); //Текст, в котором происходит ход игры.
+string x; //Здесь хранится поле внутри кода игры
+bool flag = false;
+vector<vector<char> > pole1;
+vector<vector<char> > pole2;
+string subs1;
+string subs2;
+
+void print_field(vector<vector<char> > ppole){
+    sboard << endl;
+    for(const auto& row: ppole) {
+        for(const auto& element: row) {
+            sboard << element << " ";
+        }
+        sboard << "\n";
+    }
+    sboard.close();
+}
+
+void fs_update(string now){
+    for(int i = 0; i < 10; i++){
+        int s1 = 245;
+        int s2 = 285;
+        subs1 = x.substr(s1,10);
+        subs2 = x.substr(s2, 10);
+        sboard << subs1 << endl;
+        sboard1 << subs2 << endl;
+        s1 += 60;
+        s2 += 60;
+    }
+}
+
 void disp_call(string now){
     system("clear");
     int counter = 0;
@@ -16,11 +51,17 @@ void disp_call(string now){
             if(nowx == 't'){
                 cout << "-";
             }
-            else if (nowx == 'b'){
-                cout << "X";
+            else if (nowx == 'e'){
+                cout << "·";
+            }
+            else if (nowx == 's'){
+                cout<<"*";
+            }
+            else if (nowx == 'u'){
+                cout<<"X";
             }
             else{
-                cout << "*";
+                cout << nowx;
             }
             counter++;
         }
@@ -38,10 +79,31 @@ void call_menu(){
     cout << chosen << endl;
     cout << endl;
     switch(chosen){
-        case 1:
-            getline(ofile, x);
+        case 1: {
+            getline(fdef, x);
             disp_call(x);
+            //fs_update(x);
+            //print_field(pole1);
+
+
+
+            while (flag == false) {
+                int currmove = 0; //0 - player 1 - PC
+                string currinput = "";
+                if (currmove == 0) {
+                    //стрелочка на игрока
+                    cout << endl << "Ваш ход: ";
+                    cin >> currinput;
+                    exit(0);
+
+                }
+                else {
+                    //стрелочка на комп
+                }
+
+            }
             return;
+        }
         case 2:
             cout << "continue game" << endl;
             call_menu();
